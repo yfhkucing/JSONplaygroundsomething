2b2f9bcfd827eddd16707462ca5b8fa8738076f3
yusuf <af003yusuf@gmail.com>
2022-09-16 15:36:54 +0700

    .

--- app.py
+++ app.py
@@ -1,54 +1,50 @@
 import json
 import numpy as np
 import joblib
-from urllib import request
-from flask import Flask,request
+import requests
 
-app = Flask(__name__)
-
-@app.route('/',methods=['POST'])
-
-def jalan():
-    dicti = json.loads(request.data)
-    data = dicti["data"]
-    list = []
-
-    for data in dicti["data"]:
-        list.insert(0,data["age"])
-        list.insert(1,data["sex"]) 
-        list.insert(2,data["cp"]) 
-        list.insert(3,data["trestbps"]) 
-        list.insert(4,data["chol"]) 
-        list.insert(5,data["fbs"]) 
-        list.insert(6,data["restecg"]) 
-        list.insert(7,data["thalach"]) 
-        list.insert(8,data["exang"]) 
-        list.insert(9,data["oldpeak"]) 
-        list.insert(10,data["slope"])    
-
-    m = len(dicti["data"])
-    n = len(data) - 1
-
-    arr = np.array(list) 
-    arr2 = arr.reshape(m,n)
-    model = joblib.load("randomForest_joblib.pkl")
+r = requests.get("https://belajarflaskkrestapi.herokuapp.com/")
+s = r.headers['Content-Type']
+t = r.text
 
 
-    def predict(value):
+dicti = json.loads(t)
+data = dicti["data"]
+list = []
+
+for data in dicti["data"]:
+    list.insert(0,data["age"])
+    list.insert(1,data["sex"]) 
+    list.insert(2,data["cp"]) 
+    list.insert(3,data["trestbps"]) 
+    list.insert(4,data["chol"]) 
+    list.insert(5,data["fbs"]) 
+    list.insert(6,data["restecg"]) 
+    list.insert(7,data["thalach"]) 
+    list.insert(8,data["exang"]) 
+    list.insert(9,data["oldpeak"]) 
+    list.insert(10,data["slope"])    
+
+m = len(dicti["data"])
+n = len(data) - 1
+
+arr = np.array(list) 
+arr2 = arr.reshape(m,n)
+model = joblib.load("randomForest_joblib.pkl")
+
+
+def predict(value):
         
-        pre = value.reshape(1,-1)
-        res = model.predict(pre)
-        #print(pre)
-        print(res)
-        return str(res)
+    pre = value.reshape(1,-1)
+    res = model.predict(pre)
+    #print(pre)
+    print(res)
+    return str(res)
         
 
     #print(arr2)
     #print("ini spasi")
 
-    for i in range(m):
-        predict(arr2[i])
-        i=i+1
-
-if __name__=='__main__':
-    app.run(debug=True)
+for i in range(m):
+    predict(arr2[i])
+    i=i+1
